b9c9d3ee9ef74e71e06439f9a9fe2bcddd6adb18
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2023-05-16 12:06:30 -0300

    feat:  import calculate module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { CalculateModule } from './calculate/calculate.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CalculateModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
