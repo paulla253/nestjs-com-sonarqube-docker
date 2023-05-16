1152eb6d8e14357ecda649b8a39ef77a1fb4a5d8
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2023-05-16 09:30:55 -0300

    chore: add jest-coverage

---
+++ jest-coverage.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testEnvironment: 'node',
+  testRegex: '.spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/**/*.(t|j)s', '!./src/main.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
